7bdd129864eea414a73fc0a64869e023564636a8
Vinol Fernandes <Vinolfernandes20@gmail.com>
2023-05-07 11:13:54 +0530

    made the image capturing realtime

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import './index.css'
+import React, { useState, useRef } from 'react';
+import './index.css';
+import Webcam from 'react-webcam';
 
 const FileUploadForm = () => {
   const [file, setFile] = useState(null);
@@ -21,7 +22,7 @@ const FileUploadForm = () => {
           body: formData,
         });
 
-        console.log(response)
+        console.log(response);
 
         // Handle the response from the backend
         if (response.ok) {
@@ -35,40 +36,97 @@ const FileUploadForm = () => {
     }
   };
 
-  return (
-//     <div class="container contact-form">
-//         <div class="contact-image">
-//             <img src="quantum.png" alt="rocket_contact"/>
-//         </div>
-//         <form method="post" id="imageForm">
-//             <h3>Choose an image as a seed value</h3>
-//            <div class="row">
-//                 <div class="col-md-6" style="margin:auto">
-//                     <div class="form-group">
-//                         <input type="file" name="imgFile" id="imgFile" class="form-control" placeholder="" value="" />
-//                     </div>
-//                     <div class="form-group">
-//                         <input type="submit" name="btnSubmit" class="btnContact" value="Start Process" />
-//                     </div>
-//                 </div>
-//             </div>
-//         </form>
-// </div>
-    <div >
-      <div class="container contact-form">
-        <div class="contact-image">
-            <img src="quantum.png" alt="rocket_contact"/>
+  const webcamRef = useRef(null);
+  const [capturedImage, setCapturedImage] = useState(null);
+
+  const captureImage = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setCapturedImage(imageSrc);
+
+    // Automatically select the captured image in the file input
+    const fileInput = document.getElementById('imgFile');
+    const capturedFile = dataURLtoFile(imageSrc, 'captured_image.jpg');
+    const dataTransfer = new DataTransfer();
+    dataTransfer.items.add(capturedFile);
+    fileInput.files = dataTransfer.files;
+    setFile(capturedFile);
+  };
+
+  const downloadImage = () => {
+    const link = document.createElement('a');
+    link.href = capturedImage;
+    link.download = 'captured_image.jpg';
+    link.click();
+  };
+
+  // Helper function to convert data URL to File object
+  const dataURLtoFile = (dataUrl, filename) => {
+    const arr = dataUrl.split(',');
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type: mime });
+  };
+  if(capturedImage || file){
+    return (
+      <div>
+        <div className="container contact-form">
+          <div className="contact-image">
+            <img src="quantum.png" alt="rocket_contact" />
+          </div>
+          <form onSubmit={handleSubmit} className="contact-form">
+            <h3>Choose an image as a seed value</h3>
+            <div className="row">
+              <input type="file" id="imgFile" onChange={handleFileChange} className="form-control" />
+              <div>
+                <div>
+                  <button onClick={captureImage}>Capture Image</button>
+                </div>
+                {capturedImage && (
+                  <div>
+                    <h2>Captured Image:</h2>
+                    <img src={capturedImage} alt="Captured" />
+                  </div>
+                )}
+              </div>
+              <button type="submit" className="btnContact">Start Process</button>
+            </div>
+          </form>
         </div>
-        <form onSubmit={handleSubmit} class="contact-form">
-        <h3>Choose an image as a seed value</h3>
-        <div class="row">
-        <input type="file" onChange={handleFileChange} class="form-control"/>
-          <button type="submit" class="btnContact">Start Process</button>
+      </div>  
+    )
+  }
+  else {
+    return (
+      <div>
+        <div className="container contact-form">
+          <div className="contact-image">
+            <img src="quantum.png" alt="rocket_contact" />
+          </div>
+          <form onSubmit={handleSubmit} className="contact-form">
+            <h3>Choose an image as a seed value</h3>
+            <div className="row">
+              <input type="file" id="imgFile" onChange={handleFileChange} className="form-control" />
+              <div>
+                <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+                <div>
+                  <button onClick={captureImage}>Capture Image</button>
+                  {capturedImage && (
+                    <button onClick={downloadImage}>Save Image</button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
-      </form>
-      </div>
-  </div>  
-  );
+      </div>  
+    );
+  }
+  
 };
 
-export default FileUploadForm;
+export default FileUploadForm
